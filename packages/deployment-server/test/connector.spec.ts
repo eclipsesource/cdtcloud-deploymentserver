c9df11b26423fdcd878d7e9d287664126170bb28
WoH <wolfgang.hobmaier@sensoring.de>
2021-11-28 16:59:50 +0100

    Specs: Verify open on connector queue

--- packages/deployment-server/test/connector.spec.ts
+++ packages/deployment-server/test/connector.spec.ts
@@ -6,16 +6,20 @@ import { Server } from 'node:http'
 import { PrismaClient } from '@prisma/client'
 import { closeServer, createServer } from '../src/server'
 import { Connector } from '../src/connectors'
+import WebSocket from 'ws'
+import { once } from 'node:events'
 
 const { before, teardown, test } = tap
 
 let baseUrl: string
+let port: number
 let server: Server
 let db: PrismaClient
 
 before(async () => {
   [server,,db] = await createServer()
-  const { port } = server.address() as AddressInfo
+  const address = server.address() as AddressInfo
+  port = address.port
   baseUrl = `http://0.0.0.0:${port}`
 })
 
@@ -41,8 +45,6 @@ test('Retrieves all connectors', async (t) => {
 
   const body = await response.json() as Connector[]
 
-  console.log(body, connector)
-
   t.ok(body.filter(x => x.id === connector.id))
 })
 
@@ -57,4 +59,22 @@ test('Adds a connector', async (t) => {
   const body = await response.json() as Connector
 
   t.ok(body.id)
+})
+
+test('Responds to the websocket connection after create', async (t) => {
+  t.plan(1)
+  const response = await fetch(`${baseUrl}/connectors`, {
+    method: 'POST'
+  })
+
+  const { id } = await response.json() as Connector
+
+  const socket = new WebSocket(`ws://0.0.0.0:${port}/connectors/${id}/queue`)
+
+  socket.onopen = () => {
+    t.pass('Socket opened')
+    socket.close()
+  }
+
+  await once(socket, 'close')
 })
