7ba1baff25534b897b50a9c67e143504f46008a4
Gozzim <80704304+Gozzim@users.noreply.github.com>
2021-11-28 16:44:17 +0100

    feat(Device-Connector): Add dotenv

--- packages/device-connector/src/index.ts
+++ packages/device-connector/src/index.ts
@@ -1,22 +1,27 @@
-import WebSocket from "ws"
-import { RPCClient } from "./cli-rpc/client"
+import { env } from 'process'
+import WebSocket from 'ws'
+import { RPCClient } from './cli-rpc/client'
 
 const client = await new RPCClient()
 await client.init()
 await client.createInstance()
 await client.initInstance()
 
-const registrationResponse = await fetch("http://localhost:3001/connectors", {
-  method: "POST"
+/* TODO
+const registrationResponse = await fetch('http://localhost:3001/connectors', {
+  method: 'POST'
 })
 
 const { id } = await registrationResponse.json() as any;
-
-const url = "ws://localhost:3001/connectors/" + id + "/queue"
+ */
+const address = env.SERVER_URI || '127.0.0.1:3001'
+const id = 'd6ab2191-0366-4d6d-9943-d2c50ad4f924'
+const url = 'ws://' + address + '/connectors/' + id + '/queue'
 const socket = new WebSocket(url)
 
 socket.onopen = () => {
-  socket.send("Hello")
+  console.log("Connected to " + address)
+  socket.send('Hello')
 }
 
 socket.onerror = (error) => {
@@ -24,14 +29,30 @@ socket.onerror = (error) => {
 }
 
 socket.onmessage = (e) => {
-  const message = typeof e.data === "string" ? JSON.parse(e.data) : e.data
+  const message = typeof e.data === 'string' ? JSON.parse(e.data) : e.data
   switch (message.type) {
-    case "deploy":
+    case 'deploy':
       client.uploadBin(message.deviceType, message.port, message.artifactUri)
-        .then(() => console.log("success"))
+        .then(() => console.log('success'))
         .catch((err) => console.log(err))
       break
     default:
       break
   }
 }
+
+/*
+await client.listAllBoards();
+const boards = await client.listBoards();
+if (boards) {
+  const myBoard = boards[0];
+  if (myBoard && myBoard.matching_boards) {
+    const fqbn = myBoard.matching_boards[0].fqbn;
+    const port = myBoard.port;
+    if (fqbn && port) {
+      const upload = await client.uploadBin(fqbn, port, "./tests/tests.bin");
+      console.log(upload ? "success" : "failed");
+    }
+  }
+}
+*/
