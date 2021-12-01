5c4e83b3414607c13d8850af6861ad025e1208da
Gozzim <80704304+Gozzim@users.noreply.github.com>
2021-12-01 22:30:13 +0100

    feat(ServerConnection): Proper Deployment-Server connection

--- packages/device-connector/src/index.ts
+++ packages/device-connector/src/index.ts
@@ -1,43 +1,19 @@
-import { env } from 'process'
-import WebSocket from 'ws'
 import { RPCClient } from './cli-rpc/client'
+import { openStream } from "./deployment-server/connection";
 
 const client = await new RPCClient()
 await client.init()
 await client.createInstance()
 await client.initInstance()
 
-/* TODO
-const registrationResponse = await fetch('http://localhost:3001/connectors', {
-  method: 'POST'
-})
-
-const { id } = await registrationResponse.json() as any;
- */
-const address = env.SERVER_URI != null ? env.SERVER_URI : '127.0.0.1:3001'
-const id = 'd6ab2191-0366-4d6d-9943-d2c50ad4f924'
-const url = `ws://${address}/connectors/${id}/queue`
-const socket = new WebSocket(url)
-
-socket.onopen = () => {
-  console.log(`Connected to ${address}`)
-  socket.send('Hello')
-}
-
-socket.onerror = (error) => {
-  console.log(`WebSocket error: ${error.message}`)
-}
+const socket = await openStream()
 
 socket.onmessage = (e) => {
   const message = typeof e.data === 'string' ? JSON.parse(e.data) : e.data
-  switch (message.type) {
-    case 'deploy':
+  if (message.type === 'deploy') {
       client.uploadBin(message.deviceType, message.port, message.artifactUri)
         .then(() => console.log('success'))
         .catch((err) => console.log(err))
-      break
-    default:
-      break
   }
 }
 
