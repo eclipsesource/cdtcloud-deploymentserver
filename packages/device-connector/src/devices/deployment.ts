903d640efc2d898492038cb5b43ae3846d27ab3c
Gozzim <80704304+Gozzim@users.noreply.github.com>
2021-12-02 21:35:46 +0100

    feat(Deployment): Download artifacts

--- packages/device-connector/src/devices/deployment.ts
+++ packages/device-connector/src/devices/deployment.ts
@@ -1,3 +1,7 @@
+import crypto from 'crypto'
+import { createWriteStream } from 'fs'
+import { Readable } from 'stream'
+import { request } from 'undici'
 import { Device } from './service'
 
 export interface deploymentData {
@@ -8,4 +12,16 @@ export interface deploymentData {
 export interface deploymentRequest {
   type: 'deploy'
   data: deploymentData
+}
+
+export const downloadArtifact = async (uri: string): Promise<string> => {
+  const uid = crypto.randomBytes(32).toString("hex")
+  const filePath = `artifacts/${uid}.bin`
+  const outStream = createWriteStream(filePath)
+
+  const resp = await request(uri)
+  const downStream = Readable.from(resp.body)
+  downStream.pipe(outStream)
+
+  return filePath
 }
