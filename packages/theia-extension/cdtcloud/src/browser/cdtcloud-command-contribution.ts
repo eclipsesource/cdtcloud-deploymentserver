528df67358eca4aa7db8027ebdb6f86d424356bf
WoH <wolfgang.hobmaier@sensoring.de>
2021-12-02 20:34:42 +0100

    Initial theia commands

---
+++ packages/theia-extension/cdtcloud/src/browser/cdtcloud-command-contribution.ts
@@ -0,0 +1,43 @@
+import {
+  Command,
+  CommandContribution,
+  CommandRegistry,
+} from "@theia/core/lib/common";
+import { inject, injectable } from "@theia/core/shared/inversify";
+import {
+  HelloBackendWithClientService,
+  HelloBackendService,
+} from "../common/protocol";
+
+const SayHelloViaBackendCommandWithCallBack: Command = {
+  id: "sayHelloOnBackendWithCallBack.command",
+  label: "Say hello on the backend with a callback to the client",
+};
+
+const SayHelloViaBackendCommand: Command = {
+  id: "sayHelloOnBackend.command",
+  label: "Say hello on the backend",
+};
+
+@injectable()
+export class CdtCloudCommandContribution implements CommandContribution {
+  constructor(
+    @inject(HelloBackendWithClientService)
+    private readonly helloBackendWithClientService: HelloBackendWithClientService,
+    @inject(HelloBackendService)
+    private readonly helloBackendService: HelloBackendService
+  ) {}
+
+  registerCommands(registry: CommandRegistry): void {
+    registry.registerCommand(SayHelloViaBackendCommandWithCallBack, {
+      execute: () =>
+        this.helloBackendWithClientService.greet().then((r) => console.log(r)),
+    });
+    registry.registerCommand(SayHelloViaBackendCommand, {
+      execute: () =>
+        this.helloBackendService
+          .sayHelloTo("World")
+          .then((r) => console.log(r)),
+    });
+  }
+}
--- packages/theia-extension/cdtcloud/src/browser/cdtcloud-frontend-module.ts
+++ packages/theia-extension/cdtcloud/src/browser/cdtcloud-frontend-module.ts
@@ -1,16 +1,60 @@
-import { ContainerModule } from '@theia/core/shared/inversify';
-import { CdtcloudWidget } from './cdtcloud-widget';
-import { CdtcloudContribution } from './cdtcloud-contribution';
-import { bindViewContribution, FrontendApplicationContribution, WidgetFactory } from '@theia/core/lib/browser';
+import { ContainerModule, injectable } from "@theia/core/shared/inversify";
+import { CdtcloudWidget } from "./cdtcloud-widget";
+import { CdtcloudContribution } from "./cdtcloud-contribution";
+import {
+  bindViewContribution,
+  FrontendApplicationContribution,
+  WebSocketConnectionProvider,
+  WidgetFactory,
+} from "@theia/core/lib/browser";
 
-import '../../src/browser/style/index.css';
+import "../../src/browser/style/index.css";
+import { CdtCloudCommandContribution } from "./cdtcloud-command-contribution";
+import { CommandContribution } from "@theia/core";
+import {
+  BackendClient,
+  HelloBackendService,
+  HELLO_BACKEND_PATH,
+  HelloBackendWithClientService,
+  HELLO_BACKEND_WITH_CLIENT_PATH,
+} from "../common/protocol";
 
-export default new ContainerModule(bind => {
-    bindViewContribution(bind, CdtcloudContribution);
-    bind(FrontendApplicationContribution).toService(CdtcloudContribution);
-    bind(CdtcloudWidget).toSelf();
-    bind(WidgetFactory).toDynamicValue(ctx => ({
-        id: CdtcloudWidget.ID,
-        createWidget: () => ctx.container.get<CdtcloudWidget>(CdtcloudWidget)
-    })).inSingletonScope();
+export default new ContainerModule((bind) => {
+  bindViewContribution(bind, CdtcloudContribution);
+  bind(FrontendApplicationContribution).toService(CdtcloudContribution);
+  bind(CdtcloudWidget).toSelf();
+  bind(WidgetFactory)
+    .toDynamicValue((ctx) => ({
+      id: CdtcloudWidget.ID,
+      createWidget: () => ctx.container.get<CdtcloudWidget>(CdtcloudWidget),
+    }))
+    .inSingletonScope();
+
+  bind(CommandContribution).to(CdtCloudCommandContribution).inSingletonScope();
+  bind(BackendClient).to(BackendClientImpl).inSingletonScope();
+
+  bind(HelloBackendService)
+    .toDynamicValue((ctx) => {
+      const connection = ctx.container.get(WebSocketConnectionProvider);
+      return connection.createProxy<HelloBackendService>(HELLO_BACKEND_PATH);
+    })
+    .inSingletonScope();
+
+  bind(HelloBackendWithClientService)
+    .toDynamicValue((ctx) => {
+      const connection = ctx.container.get(WebSocketConnectionProvider);
+      const backendClient: BackendClient = ctx.container.get(BackendClient);
+      return connection.createProxy<HelloBackendWithClientService>(
+        HELLO_BACKEND_WITH_CLIENT_PATH,
+        backendClient
+      );
+    })
+    .inSingletonScope();
 });
+
+@injectable()
+class BackendClientImpl implements BackendClient {
+  async getName(): Promise<string> {
+    return "Theia";
+  }
+}
