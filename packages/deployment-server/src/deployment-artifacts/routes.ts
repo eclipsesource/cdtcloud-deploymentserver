69886163ded512bdb65203c3b51317ca33d8706d
WoH <wolfgang.hobmaier@sensoring.de>
2021-12-02 11:55:35 +0100

    Artifacts: Export interfaces, verify

--- packages/deployment-server/src/deployment-artifacts/routes.ts
+++ packages/deployment-server/src/deployment-artifacts/routes.ts
@@ -5,6 +5,7 @@ import { createReadStream } from 'fs'
 import { pipeline } from 'stream/promises'
 import { validate } from '../util/validate'
 import multer from 'multer'
+import { CreateArtifactResponseBody } from '.'
 
 export default function deploymentArtifactsRoutes (router: Router): void {
   const upload = multer({
@@ -16,37 +17,51 @@ export default function deploymentArtifactsRoutes (router: Router): void {
     })
   })
 
-  router.post('/deployment-artifacts', upload.single('file'), async (req, res, next) => {
-    try {
-      if (req.file == null) {
-        return res.sendStatus(400)
-      }
+  router.post(
+    '/deployment-artifacts',
+    upload.single('file'),
+    validate<CreateArtifactResponseBody>({}),
+    async (req, res, next) => {
+      try {
+        if (req.file == null) {
+          return res.sendStatus(400)
+        }
 
-      const artifactUri = `${req.protocol}://${req.get('host') ?? ''}${req.originalUrl}/${req.file.filename}`
-      return res.json({ artifactUri })
-    } catch (e) {
-      console.error(e)
-      next(e)
+        const host = req.get('host')
+
+        if (host == null) {
+          return res.sendStatus(500)
+        }
+
+        const { protocol, originalUrl: url } = req
+
+        const artifactUri =
+          `${protocol}://${host}${url}/${req.file.filename}`
+        return res.json({ artifactUri })
+      } catch (e) {
+        next(e)
+      }
     }
-  })
+  )
 
   const params = Type.Object({
     artifactId: Type.String({ format: 'uuid' })
   }, { additionalProperties: false })
 
-  router.get('/deployment-artifacts/:artifactId', validate<unknown, Static<typeof params>>({ params }), async (req, res, next) => {
-    try {
-      const filename = req.params.artifactId
+  router.get(
+    '/deployment-artifacts/:artifactId',
+    validate<unknown, Static<typeof params>>({ params }),
+    async (req, res, next) => {
+      try {
+        const filename = req.params.artifactId
 
-      // validation rejects escape attempts
-      const filePath = `uploads/${filename}`
+        // validation rejects escape attempts
+        const filePath = `uploads/${filename}`
 
-      return await pipeline(
-        createReadStream(filePath),
-        res
-      )
-    } catch (e) {
-      next(e)
+        return await pipeline(createReadStream(filePath), res)
+      } catch (e) {
+        next(e)
+      }
     }
-  })
+  )
 }
