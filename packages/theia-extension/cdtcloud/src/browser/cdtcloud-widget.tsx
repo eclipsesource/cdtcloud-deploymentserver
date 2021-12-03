54ef377d1bb16bdb8cf0c6e51413a687e71f776b
Zaim Sari <zaim.sari@tum.de>
2021-12-03 20:39:14 +0100

    Frontend for Durchstich

--- packages/theia-extension/cdtcloud/src/browser/cdtcloud-widget.tsx
+++ packages/theia-extension/cdtcloud/src/browser/cdtcloud-widget.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Select from 'react-select'
+import Select from 'react-select';
 import { injectable, postConstruct, inject } from '@theia/core/shared/inversify';
 import { AlertMessage } from '@theia/core/lib/browser/widgets/alert-message';
 import { ReactWidget } from '@theia/core/lib/browser/widgets/react-widget';
@@ -27,80 +27,51 @@ export class CdtcloudWidget extends ReactWidget {
         this.update();
     }
     
-    
-/*
-    private async getDevices() {
-        const response = await fetch("localhost:3001/device-types");
-        console.log(response);
-        const data = await response.json();
-        console.log(data);
-    }
-    */
-    
 
     
 
     render(): React.ReactElement {
-        const header = `This widget enables you to deploy your code on a remote (Arduino-)board.`;
 
+        const header = `This widget enables you to deploy your code on a remote (Arduino-)board.`;
+        
         const options = [
             { value: 'uniqueId1', label: 'Arduino Uno' },
             { value: 'uniqueId2', label: 'Arduino Due' },
             { value: 'uniqueId3', label: 'Arduino Trio' }
           ]
-        
-         
-         /*
-        const numbers = [1, 2, 3, 4, 5];
-        const listItems = numbers.map((number) =>
-        <li key={number.toString()}>
-                {number}
-            </li>
-        );
-        
-       
-        const myBoards =  ["Arduino Uno ", "Arduino Due ", "Arduino Trio "];
-        const BoardList = myBoards.map((myBoards) =>
-        <li key={myBoards.toString()}>
-            {myBoards}
-        </li>
-        );
-        */
 
         return <div id='widget-container'>
-            <AlertMessage type='INFO' header={header} / >
+             <AlertMessage type='INFO' header={header} />
             
             <h2> Select a Board to deploy your code on from this list</h2>
 
             <Select options={options} />
-
-            <hr></hr>
-
-            {/* <button className='theia-button secondary' title='Refresh Devices' onClick={_a => this.refreshDevices()}>Refresh Devices</button> */}
-            <button className='theia-button secondary' title='Deploy' onClick={_a => this.deployOnBoard()}>Deploy on Board</button>
+            <button className='theia-button secondary' title='Display Message' onClick={_a => this.deployOnBoard()}>Deploy on Board</button>
         </div>
     }
 
-    protected displayMessage(): void {
-        this.messageService.info('Congratulations: Button Pressed!');
-
-    }
-
-    protected refreshDevices(): void {
-        // Refresh devices
-        // add some logic here 
-        this.messageService.info('Refresh Devices');
-
-    }
-    
-    protected deployOnBoard(): void{
-        // Deploy
-        // add some logic here 
+    protected async deployOnBoard(): Promise<void> {
         
-        this.messageService.info('Deploying on Board x')
+        const data = { deviceTypeId: '37dcb3ab-071d-43e1-935a-1e70403e4720', artifactUri: 'https://sanctum-dev.com/tests.bin'};
+
+        const response = await fetch('http://127.0.0.1:3001/deployments', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+          });
+ 
+         // const result = await response.json() as any
+
+         // console.log(result);
+
+          this.messageService.info('Deployment Request send:');
     }
 
+    
 
-     
+   
+
 
 }
