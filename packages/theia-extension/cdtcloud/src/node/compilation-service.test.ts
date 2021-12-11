5605ad885369112e297bec595a367c688e73e9c1
WoH <wolfgang.hobmaier@sensoring.de>
2021-12-11 13:32:27 +0100

    fix(theia-be): Working form data

--- packages/theia-extension/cdtcloud/src/node/compilation-service.test.ts
+++ packages/theia-extension/cdtcloud/src/node/compilation-service.test.ts
@@ -1,19 +1,23 @@
-import { CompilationServiceImpl } from './compilation-service';
-import 'reflect-metadata';
+import { CompilationServiceImpl } from "./compilation-service";
+import "reflect-metadata";
 
-describe('CompilationServiceImpl', () => {
+describe("CompilationServiceImpl", () => {
+  let compilationServiceImpl: CompilationServiceImpl;
 
-    let compilationServiceImpl: CompilationServiceImpl;
+  beforeEach(async () => {
+    compilationServiceImpl = new CompilationServiceImpl();
+  });
 
-    beforeEach(async () => {
-        compilationServiceImpl = new CompilationServiceImpl();
-    });
-
-    it('should log three strings in console', async () => {
-        try { await compilationServiceImpl.compile("arduino:avr:mega", "e8665d51-4221-4d3c-b892-a672a2af37a2", "C:\\Users\\kevin\\Documents\\Arduino\\Light_Project_1\\Light_Project_1.ino")
-    } catch (err){
-        console.error(err)
+  it("should log three strings in console", async () => {
+    try {
+      await compilationServiceImpl.compile(
+        "arduino:avr:mega",
+        "e8665d51-4221-4d3c-b892-a672a2af37a2",
+        "C:\\Users\\kevin\\Documents\\Arduino\\Light_Project_1\\Light_Project_1.ino"
+      );
+    } catch (err) {
+      console.error(err);
     }
-        expect(true).toBe(false)
-    }, 20000);
+    expect(true).toBe(false);
+  }, 20000);
 });
--- packages/theia-extension/cdtcloud/src/node/compilation-service.ts
+++ packages/theia-extension/cdtcloud/src/node/compilation-service.ts
@@ -2,16 +2,16 @@ import { injectable } from "inversify";
 import { CompilationService } from "../common/protocol";
 import { readdir } from 'fs/promises';
 import { createReadStream } from 'fs';
-import axios from 'axios';
 import { RPCClient } from "./rpc-client";
 import 'reflect-metadata';
 import FormData from 'form-data';
 import { join } from 'path';
+import got from "got";
 
 
 @injectable()
 export class CompilationServiceImpl implements CompilationService {
-  
+
   binaryFile: string;
   binaryFileContent: Buffer;
   artifactUri: string;
@@ -26,37 +26,45 @@ export class CompilationServiceImpl implements CompilationService {
 
 
       console.log(buildPath)
-  
+
       const files = await readdir(buildPath)
 
       files.forEach((file) => {
           if(file.endsWith('.bin')){
               this.binaryFile = join(buildPath, file)
           }
-      }) 
+      })
 
-      console.log('binary file: ' + this.binaryFile)
 
       let form = new FormData();
       const content = createReadStream(this.binaryFile)
       form.append('file', content)
       const formHeaders = form.getHeaders();
-      console.log(formHeaders)
 
-      await new Promise<void>((resolve, reject) => {
-          form.getLength(async (err, length) => {
-          const response = await axios.post(`http://localhost:3001/deployment-artifacts`, form, {headers: {...formHeaders, "Content-Length": "" + 261657}, validateStatus:  () => true})
-          
-          console.log(response.data)
-          this.artifactUri = response.data.artifactUri
-
-          console.log(this.artifactUri)
-          const data = {'deviceTypeId': id, 'artifactUri': this.artifactUri}
-          const resp = await axios.post(`http://localhost:3001/deployments`, data, {headers:{"Content-Type": "application/json"}})
-          console.log(resp) 
-          resolve()
+      const uploadResponse =
+        await got.post<{artifactUri: string}>(`http://localhost:3001/deployment-artifacts`, {
+          headers: {
+            ...formHeaders
+          },
+          body: form,
+          responseType: 'json'
         })
-      })
-           
+
+        const artifactUri = uploadResponse.body.artifactUri
+
+        const deploymentResponse =
+          await got.post(`http://localhost:3001/deployments`, {
+            headers:{
+              "Content-Type": "application/json"
+            },
+            responseType: 'json',
+            body: JSON.stringify({
+              artifactUri,
+              deviceTypeId: id
+          })})
+
+
+        console.log(deploymentResponse.body)
+
   }
 }
