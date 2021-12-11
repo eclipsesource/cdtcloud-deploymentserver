f3379c0de3f7b737be2ebd4e55188470b4bacd39
Kevin Kusterer <k.kusterer@tum.de>
2021-12-11 16:43:34 +0100

    merge widget from main

--- packages/theia-extension/cdtcloud/src/browser/cdtcloud-widget.tsx
+++ packages/theia-extension/cdtcloud/src/browser/cdtcloud-widget.tsx
@@ -1,30 +1,20 @@
-import * as React from "react";
-import {
-  injectable,
-  postConstruct,
-  inject,
-} from "@theia/core/shared/inversify";
-import { AlertMessage } from "@theia/core/lib/browser/widgets/alert-message";
-import { ReactWidget } from "@theia/core/lib/browser/widgets/react-widget";
-import { MessageService } from "@theia/core";
-import { DeviceTypeService } from "../common/protocol";
+import * as React from 'react';
+import Select from 'react-select';
+import { injectable, postConstruct, inject } from '@theia/core/shared/inversify';
+import { AlertMessage } from '@theia/core/lib/browser/widgets/alert-message';
+import { ReactWidget } from '@theia/core/lib/browser/widgets/react-widget';
+import { MessageService } from '@theia/core';
 
 
 @injectable()
 export class CdtcloudWidget extends ReactWidget {
-  static readonly ID = "cdtcloud:widget";
-  static readonly LABEL = "Cdtcloud Widget";
+
+  static readonly ID = 'cdtcloud:widget';
+  static readonly LABEL = 'Cdtcloud Widget';
 
   @inject(MessageService)
   protected readonly messageService!: MessageService;
 
-  constructor(
-    @inject(DeviceTypeService)
-    private readonly deviceTypeService: DeviceTypeService, 
-   
-  ) {
-    super();
-  }
 
 
   @postConstruct()
@@ -33,31 +23,55 @@ export class CdtcloudWidget extends ReactWidget {
     this.title.label = CdtcloudWidget.LABEL;
     this.title.caption = CdtcloudWidget.LABEL;
     this.title.closable = true;
-    this.title.iconClass = "fa fa-window-maximize"; // example widget icon.
-
+    this.title.iconClass = 'fa fa-window-maximize';
     this.update();
   }
 
+
+
+
   render(): React.ReactElement {
-    const header = `This is a widget which simply calls the messageService
-        in order to display an info message to end users.`;
-    return (
-      <div id="widget-container">
-        <AlertMessage type="INFO" header={header} />
-        <button
-          className="theia-button secondary"
-          title="Display Message"
-          onClick={(_a) => this.displayMessage()}
-        >
-          Display Message
-        </button>
-      </div>
-    );
+
+    const header = `This widget enables you to deploy your code on a remote (Arduino-)board.`;
+
+    //hier availableDevices einfügen
+    const options = [
+      { value: 'uniqueId1', label: 'Arduino Uno' },
+      { value: 'uniqueId2', label: 'Arduino Due' },
+      { value: 'uniqueId3', label: 'Arduino Trio' }
+    ]
+
+    return <div id='widget-container'>
+      <AlertMessage type='INFO' header={header} />
+
+      <h2> Select a Board to deploy your code on from this list</h2>
+
+      <Select options={options} />
+      <button className='theia-button secondary' title='Display Message' onClick={_a => this.deployOnBoard()}>Deploy on Board</button>
+    </div>
   }
 
-  protected async displayMessage(): Promise<void> {
-    this.deviceTypeService.getDeviceList().then((result) => {
-      this.messageService.info(JSON.stringify(result[0]));
-    });    
+  protected async deployOnBoard(): Promise<void> {
+
+    const data = { deviceTypeId: '37dcb3ab-071d-43e1-935a-1e70403e4720', artifactUri: 'https://sanctum-dev.com/tests.bin' };
+
+    try {
+      await fetch('http://127.0.0.1:3001/deployments', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+    }
+    catch { }
+
+    this.messageService.info('Deployment Request send.');
   }
+
+
+
+
+
+
 }
