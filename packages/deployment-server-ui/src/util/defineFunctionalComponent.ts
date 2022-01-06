b1d0f6e7a04820e0ed7132b47592e8757bc89a08
WoH <wolfgang.hobmaier@sensoring.de>
2022-01-07 00:10:31 +0100

    feat(server-ui): Add defineFunctionalComponent helper

---
+++ packages/deployment-server-ui/src/util/defineFunctionalComponent.ts
@@ -0,0 +1,8 @@
+import { FunctionComponent } from "react";
+
+export type Props<F> = F extends FunctionComponent<infer P> ? P : never;
+
+export function defineFunctionalComponent<P = {},>(f: FunctionComponent<P>): FunctionComponent<P> {
+  return f;
+}
+export default defineFunctionalComponent;
