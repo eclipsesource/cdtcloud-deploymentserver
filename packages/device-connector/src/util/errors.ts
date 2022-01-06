30c320732909ad9f9d91122febfe59b02fc1692b
Gozzim <80704304+Gozzim@users.noreply.github.com>
2022-01-06 14:54:02 +0100

    feat(DeviceConnector/Logger): Readable time format, grpcStatus formatter & pino update

--- packages/device-connector/src/util/errors.ts
+++ packages/device-connector/src/util/errors.ts
@@ -1,13 +1,13 @@
 import { StatusObject } from '@grpc/grpc-js'
-import { GrpcStatus } from './common'
-
-export const GrpcError = {
-  notInitialized: new Error(GrpcStatus.notInitialized),
-  unknown: new Error(GrpcStatus.unknown)
-}
+import { Status } from '@grpc/grpc-js/build/src/constants'
 
 export const grpcStatusToError = (status: StatusObject): string => {
   const details: string = status.details === '' ? (status.code === 0 ? 'Success' : 'Unknown error') : status.details
+  const statusCode = grpcStatusCodeToName(status.code)
 
-  return `${details} (code: ${status.code})`
+  return `${details} (code: ${status.code} - ${statusCode})`
+}
+
+export const grpcStatusCodeToName = (code: Status): string => {
+  return Status[code]
 }
--- packages/device-connector/src/util/logger.ts
+++ packages/device-connector/src/util/logger.ts
@@ -1,28 +1,32 @@
 import { env } from 'process'
-import pino, { LogFn } from 'pino'
+import pino from 'pino'
 import { grpcStatusToError } from './errors'
+import { StatusObject } from '@grpc/grpc-js'
 
 const statusObjectKeys = ['code', 'details', 'metadata'].toString()
 
-const hooks = {
-  logMethod (inputArgs: any[], method: LogFn) {
-    const args = inputArgs.map((arg) => {
-      if (Object.keys(arg).toString() === statusObjectKeys) {
-        return grpcStatusToError(arg)
-      }
-
-      return arg
-    })
-    return method.apply(this, [args[0], ...args])
+const formatters = {
+  log (object: object) {
+    if (Object.keys(object).toString() === statusObjectKeys) {
+      return { msg: grpcStatusToError(object as StatusObject) }
+    }
+    return object
   }
 }
 
-export const logger = pino({
-  name: 'DeviceConnector',
-  prettyPrint: env.NODE_ENV === 'development',
-  level: env.NODE_ENV === 'test' ? 'fatal' : 'info',
-  hooks
+const transport = {
+  target: 'pino-pretty',
+  options: {
+    colorize: env.NODE_ENV === 'development' ,
+    translateTime: 'SYS:yyyy-mm-dd HH:MM:ss.l'
+  }
 }
-)
+
+const logger = pino({
+  name: 'DeviceConnector',
+  transport,
+  level: env.NODE_ENV === 'test' ? 'fatal' : 'info',
+  formatters
+})
 
 export default logger
