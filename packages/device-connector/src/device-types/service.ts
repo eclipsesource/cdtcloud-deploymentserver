f550e882ed85cc6d5e4b344ea4ffb7f81c51f908
Gozzim <80704304+Gozzim@users.noreply.github.com>
2022-01-06 16:19:15 +0100

    feat(DeviceConnector): Add device-types store and service

---
+++ packages/device-connector/src/device-types/service.ts
@@ -0,0 +1,43 @@
+import { sendNewDeviceTypeRequest } from '../deployment-server/service'
+import { DeviceTypes } from './store'
+import logger from '../util/logger'
+
+export type FQBN = string
+
+export interface DeviceType {
+  id: string
+  name: string
+  fqbn: FQBN
+}
+
+export const getRemoteDeviceType = async (fqbn: FQBN, name: string): Promise<DeviceType> => {
+  await DeviceTypes.update()
+  let deviceType = DeviceTypes.withFQBN(fqbn)
+
+  if (deviceType == null) {
+    logger.warn(`DeviceType with fqbn ${fqbn} not found.`)
+    deviceType = await registerNewDeviceType(fqbn, name)
+  }
+
+  return deviceType
+}
+
+export const getDeviceTypeId = async (fqbn: FQBN, name: string): Promise<string> => {
+  let deviceType = await DeviceTypes.withFQBN(fqbn)
+
+  if (deviceType == null) {
+    deviceType = await getRemoteDeviceType(fqbn, name)
+  }
+
+  return deviceType.id
+}
+
+export const registerNewDeviceType = async (fqbn: FQBN, name: string): Promise<DeviceType> => {
+  try {
+    logger.info(`Trying to register new DeviceType ${name} with fqbn ${fqbn}`)
+    return await sendNewDeviceTypeRequest(fqbn, name)
+  } catch (e) {
+    logger.error(e)
+    throw e
+  }
+}
