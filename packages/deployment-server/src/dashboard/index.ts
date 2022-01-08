d35092138882956eaaecc0de493d9ac26ec304fd
WoH <wolfgang.hobmaier@sensoring.de>
2022-01-08 15:24:12 +0100

    feat(deployment-server): Dashboard types

---
+++ packages/deployment-server/src/dashboard/index.ts
@@ -0,0 +1,17 @@
+import { Device } from '@prisma/client'
+import { DeployRequest, DeviceType } from '..'
+
+export type RecentDeployment = (DeployRequest & {
+  device: Device & {
+    type: DeviceType
+  }
+})
+
+export interface Dashboard {
+  recentDeployments: RecentDeployment[]
+  deployRequestCount: number
+  deviceCount: number
+  deploymentOverview: Record<DeployRequest['status'], number>
+  deviceOverview: Record<Device['status'], number>
+
+}
--- packages/deployment-server/src/dashboard/routes.ts
+++ packages/deployment-server/src/dashboard/routes.ts
@@ -1,10 +1,12 @@
 import { DeployStatus, DeviceStatus } from '@prisma/client'
 import { Router } from 'express'
+import { Dashboard } from '.'
+import { validate } from '../util/validate'
 
 export default function dashboardRoutes (router: Router): void {
   router.get(
     '/dashboard',
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    validate<Dashboard>({}),
     async (req, res, next) => {
       try {
         const recentDeployments = await req.db.deployRequest.findMany({
