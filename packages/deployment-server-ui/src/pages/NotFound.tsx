91133c6e7e1541d83194ead996a1d66403d536eb
WoH <wolfgang.hobmaier@sensoring.de>
2022-01-09 14:40:35 +0100

    feat(deployment-ui): 404 page

---
+++ packages/deployment-server-ui/src/pages/NotFound.tsx
@@ -0,0 +1,19 @@
+import { Button, Result } from "antd";
+import { useNavigate } from "react-router-dom";
+import defineFunctionalComponent from "../util/defineFunctionalComponent";
+
+export default defineFunctionalComponent(function NotFound() {
+  const navigate = useNavigate();
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button type="primary" onClick={() => navigate("/")}>
+          Back Home
+        </Button>
+      }
+    />
+  );
+});
