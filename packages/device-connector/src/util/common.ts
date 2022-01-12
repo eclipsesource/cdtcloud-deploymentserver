0baaba8dc97123377ca5d26e4869de0ac706a3fc
Gozzim <80704304+Gozzim@users.noreply.github.com>
2022-01-12 14:57:42 +0100

    style(DeviceConnector): Parse enums to const

--- packages/device-connector/src/util/common.ts
+++ packages/device-connector/src/util/common.ts
@@ -1,14 +1,14 @@
-export enum DeviceStatus {
-  UNAVAILABLE = 'UNAVAILABLE',
-  AVAILABLE = 'AVAILABLE',
-  DEPLOYING = 'DEPLOYING',
-  RUNNING = 'RUNNING'
-}
+export const DeviceStatus = {
+  UNAVAILABLE: 'UNAVAILABLE',
+  AVAILABLE: 'AVAILABLE',
+  DEPLOYING: 'DEPLOYING',
+  RUNNING: 'RUNNING'
+} as const
 
-export enum DeployStatus {
-  PENDING = 'PENDING',
-  RUNNING = 'RUNNING',
-  SUCCESS = 'SUCCESS',
-  FAILED = 'FAILED',
-  TERMINATED = 'TERMINATED'
-}
+export const DeployStatus = {
+  PENDING: 'PENDING',
+  RUNNING: 'RUNNING',
+  SUCCESS: 'SUCCESS',
+  FAILED: 'FAILED',
+  TERMINATED: 'TERMINATED'
+} as const
