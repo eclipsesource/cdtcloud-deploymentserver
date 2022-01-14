3e4369d177e239428cca659ebec0b5b1ce3733e1
Zaim Sari <zaim.sari@tum.de>
2022-01-14 18:26:59 +0100

    Removed Empty Logo

--- packages/deployment-server-ui/src/components/CdtCloudHeader.tsx
+++ packages/deployment-server-ui/src/components/CdtCloudHeader.tsx
@@ -4,7 +4,6 @@ import "./CdtCloudHeader.css";
 export function CdtCloudHeader() {
   return (
     <Header className="header">
-      <div className="logo" />
       <h2 style={{ color: "rgba(255, 255, 255, 0.65)", textAlign: "center" }}>
         Embedded Cloud Development - Admin Dashboard 
       </h2>
