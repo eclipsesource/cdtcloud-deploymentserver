bd1c92b882f9894a4ea492828ae70728798d3104
Zaim Sari <zaim.sari@tum.de>
2022-01-16 13:38:22 +0100

    feat:dashboard UI cleanup

--- packages/deployment-server-ui/src/pages/Dashboard.tsx
+++ packages/deployment-server-ui/src/pages/Dashboard.tsx
@@ -5,11 +5,10 @@ import { Dashboard } from "deployment-server";
 import { useState, useEffect } from "react";
 import { useInterval } from "react-use";
 
-import { Card, Divider,  Row, Col } from "antd";
+import { Card, Divider, Row, Col } from "antd";
 import "./DashboardCSS.css";
 
 export default defineFunctionalComponent(function Dasboard() {
-
   const [data, setData] = useState<Dashboard>();
   let [refreshFlip, setRefetchFlip] = useState(false);
 
@@ -23,45 +22,41 @@ export default defineFunctionalComponent(function Dasboard() {
       .then((res) => setData(res));
   }, [refreshFlip]);
 
-
   return (
     <main>
       <h2> Dashboard </h2>
       <Divider />
-
-      <div className="gutter-example">
-        <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 20]}>
-          <Col span={8}>
-            <Card title="Total Deployments" >
-              <h2>{data?.deployRequestCount}</h2>
-            </Card>
-          </Col>
-
-          <Col span={8}>
-            <Card title="Connected Devices" >
-              <h2>{data?.deviceCount}</h2>
-            </Card>
-          </Col>
-          <Col span={8}>
-            <Card title="Supported Board Types" >
-              <h2>Memes</h2>
-            </Card>
-          </Col>
-        </Row>
-
-        <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 20]}>
-          <Col span={14}>
-            <Card title="Deployments over Time Chart" >
-             <DeploymentsOverTimeGraph/>
-            </Card>
-          </Col>
-          <Col span={10}>
-            <Card title="Recent Deployments " >
-             <RecentDeploymentList/>
-            </Card>
-          </Col>
-        </Row>
-      </div>
+      <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 20]}>
+        <Col span={8}>
+          <Card title="Total Deployments">
+            <h2>{data?.deployRequestCount}</h2>
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card title="Connected Devices">
+            <h2>{data?.deviceCount}</h2>
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card title="Supported Board Types">
+            <h2>Memes</h2>
+          </Card>
+        </Col>
+      </Row>
+      <Divider />
+      <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 20]} align="middle">
+        <Col span={12}>
+          <Card title="Deployments over Time Chart">
+            <DeploymentsOverTimeGraph />
+          </Card>
+        </Col>
+        <Col span={12}>
+          <Card title="Recent Deployments ">
+            <RecentDeploymentList />
+          </Card>
+        </Col>
+      </Row>
+      <Divider />
     </main>
   );
 });
