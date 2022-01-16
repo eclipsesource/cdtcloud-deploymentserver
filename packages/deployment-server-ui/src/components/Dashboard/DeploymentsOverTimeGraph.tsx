54f7e66c03fb72f82f8be19029e000eae7f18d2b
Zaim Sari <zaim.sari@tum.de>
2022-01-16 13:37:13 +0100

    feat:dashboard feed Graph real data

--- packages/deployment-server-ui/src/components/Dashboard/DeploymentsOverTimeGraph.tsx
+++ packages/deployment-server-ui/src/components/Dashboard/DeploymentsOverTimeGraph.tsx
@@ -2,27 +2,29 @@ import { useState, useEffect } from 'react';
 import { Line } from '@ant-design/plots';
 import defineFunctionalComponent from '../../util/defineFunctionalComponent';
 
+type DataEntries = {xField: string, yField: string}[]
+
 export default defineFunctionalComponent(function DeploymentsOverTimeGraph() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<DataEntries>([]);
 
   useEffect(() => {
     asyncFetch();
   }, []);
 
   const asyncFetch = () => {
-    fetch('https://gw.alipayobjects.com/os/bmw-prod/1d565782-dde4-4bb6-8946-ea6a38ccf184.json')
+    fetch('http://localhost:3001/api/dashboard')
       .then((res) => res.json())
-      .then((json) => setData(json));
+      .then((json) => setData(Object.entries<string>(json.deploymentsPerBucket).reduce<DataEntries>((acc,[key,value]) => ([...acc, ({ xField: key, yField: value })]), [])))
   };
 
   const config = {
     data,
     padding: "auto" as const,
-    xField: 'Date',
-    yField: 'scales',
+    xField: 'xField',
+    yField: 'yField',
     xAxis: {
       // type: 'timeCat',
-      tickCount: 5,
+      tickCount: 1,
     },
     smooth: true,
   };
