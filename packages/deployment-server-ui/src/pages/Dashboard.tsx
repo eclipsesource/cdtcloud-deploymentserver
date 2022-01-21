da52b65fa5e75d9dc70a9d84a04d3b95a1e0a725
Zaim Sari <zaim.sari@tum.de>
2022-01-21 20:51:56 +0100

    feat(dashboard): removed "Dashboard" headline to gain more visual space + code clean up

--- packages/deployment-server-ui/src/pages/Dashboard.tsx
+++ packages/deployment-server-ui/src/pages/Dashboard.tsx
@@ -7,7 +7,21 @@ import { useInterval } from "react-use";
 
 import { Card, Divider, Row, Col, Tabs, Statistic } from "antd";
 import "./Dashboard.css";
-import { CheckCircleOutlined, CheckSquareOutlined, CloseCircleOutlined, CloudSyncOutlined, ExclamationCircleOutlined, FieldTimeOutlined, HddOutlined, LockOutlined, PauseCircleOutlined, PlayCircleOutlined, PoweroffOutlined, RocketOutlined, SyncOutlined } from "@ant-design/icons";
+import {
+  CheckCircleOutlined,
+  CheckSquareOutlined,
+  CloseCircleOutlined,
+  CloudSyncOutlined,
+  ExclamationCircleOutlined,
+  FieldTimeOutlined,
+  HddOutlined,
+  LockOutlined,
+  PauseCircleOutlined,
+  PlayCircleOutlined,
+  PoweroffOutlined,
+  RocketOutlined,
+  SyncOutlined,
+} from "@ant-design/icons";
 
 export default defineFunctionalComponent(function Dasboard() {
   const [data, setData] = useState<Dashboard>();
@@ -25,8 +39,6 @@ export default defineFunctionalComponent(function Dasboard() {
 
   return (
     <main>
-      <h2> Dashboard </h2>
-      <Divider />
       <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 20]} align="stretch">
         <Col span={8}>
           <Card
@@ -40,13 +52,13 @@ export default defineFunctionalComponent(function Dasboard() {
                   title="Total Deploys"
                   value={data?.deployRequestCount ?? 0}
                   prefix={<RocketOutlined />}
-                  valueStyle={{ color: 'black' }}
+                  valueStyle={{ color: "black" }}
                 />
                 <Statistic
                   title="Pending Deploys"
                   value={data?.deploymentOverview.PENDING ?? 0}
                   prefix={<FieldTimeOutlined />}
-                  valueStyle={{ color: 'blue' }}
+                  valueStyle={{ color: "blue" }}
                 />
               </Col>
               <Col span={8}>
@@ -54,13 +66,13 @@ export default defineFunctionalComponent(function Dasboard() {
                   title="Running Deploys"
                   value={data?.deployRequestCount ?? 0}
                   prefix={<PlayCircleOutlined />}
-                  valueStyle={{ color: 'grey' }}
+                  valueStyle={{ color: "grey" }}
                 />
                 <Statistic
                   title="Success Deploys"
                   value={data?.deploymentOverview.SUCCESS ?? 0}
                   prefix={<CheckCircleOutlined />}
-                  valueStyle={{ color: 'green' }}
+                  valueStyle={{ color: "green" }}
                 />
               </Col>
               <Col span={8}>
@@ -68,13 +80,13 @@ export default defineFunctionalComponent(function Dasboard() {
                   title="Terminated Deploys"
                   value={data?.deploymentOverview.TERMINATED ?? 0}
                   prefix={<ExclamationCircleOutlined />}
-                  valueStyle={{ color: 'orange' }}
+                  valueStyle={{ color: "orange" }}
                 />
                 <Statistic
                   title="Failed Deploys"
                   value={data?.deploymentOverview.FAILED ?? 0}
                   prefix={<CloseCircleOutlined />}
-                  valueStyle={{ color: 'red' }}
+                  valueStyle={{ color: "red" }}
                 />
               </Col>
             </Row>
@@ -92,13 +104,13 @@ export default defineFunctionalComponent(function Dasboard() {
                   title="Available Devices"
                   value={data?.deviceOverview.AVAILABLE ?? 0}
                   prefix={<HddOutlined />}
-                  valueStyle={{ color: 'green' }}
+                  valueStyle={{ color: "green" }}
                 />
                 <Statistic
                   title="Deploying Devices"
                   value={data?.deviceOverview.DEPLOYING ?? 0}
                   prefix={<SyncOutlined spin />}
-                  valueStyle={{ color: 'blue' }}
+                  valueStyle={{ color: "blue" }}
                 />
               </Col>
               <Col span={12}>
@@ -106,13 +118,13 @@ export default defineFunctionalComponent(function Dasboard() {
                   title="Running Devices"
                   value={data?.deviceOverview.RUNNING ?? 0}
                   prefix={<PlayCircleOutlined />}
-                  valueStyle={{ color: 'grey' }}
+                  valueStyle={{ color: "grey" }}
                 />
                 <Statistic
                   title="Unavailable Devices"
                   value={data?.deviceOverview.UNAVAILABLE ?? 0}
                   prefix={<LockOutlined />}
-                  valueStyle={{ color: 'red' }}
+                  valueStyle={{ color: "red" }}
                 />
               </Col>
             </Row>
