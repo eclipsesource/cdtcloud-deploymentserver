254e887d2352b42c0127e959284195e12e2c9515
Zaim Sari <zaim.sari@tum.de>
2022-01-26 19:55:57 +0100

    feat(deployment-server-ui) updates to types

--- packages/deployment-server-ui/src/pages/Types.tsx
+++ packages/deployment-server-ui/src/pages/Types.tsx
@@ -8,6 +8,7 @@ import { defineFunctionalComponent } from "../util/defineFunctionalComponent";
 import SkeletonButton from "antd/lib/skeleton/Button";
 import MegaTest from "../ressources/mega.svg";
 import { AutoComplete, Row, Col } from "antd";
+import DeploymentsByTypeGraph from "../components/Graphs/DeploymentsByTypeGraph";
 
 const { Meta } = Card;
 
@@ -22,30 +23,14 @@ export default defineFunctionalComponent(function Types() {
     Array(15).fill({})
   );
 
-  const options = [
-    {
-      value: "Burns Bay Road",
-    },
-    {
-      value: "Downing Street",
-    },
-    {
-      value: "Wall Street",
-    },
-  ];
+  const [graphData, setGraphData] = useState([]);
 
-  const Complete = () => (
-    <AutoComplete
-      style={{
-        width: 250,
-      }}
-      options={types}
-      placeholder="type to search`"
-      filterOption={(inputValue, option) =>
-        option!.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
-      }
-    />
-  );
+  useEffect(() => {
+    fetch("/api/deployments/")
+      .then((res) => res.json())
+      .then((res) => setGraphData(res.history));
+  }, []);
+
 
   useEffect(() => {
     fetch("/api/device-types")
@@ -55,101 +40,80 @@ export default defineFunctionalComponent(function Types() {
 
   return (
     <>
-      <h2>Board Type Overview</h2>
-      <Divider />
-      <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 20]}  align="middle">
-        <Col span={6}>
-          <Card
-            title="Search Board Types"
-            style={{ height: "100%" }}
-          >
-            <Complete />
+      <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 20]} align="middle">
+        <Col span={12}>
+          <Card title="Graph 1" style={{ height: "100%" }}>
+            <DeploymentsByTypeGraph data={graphData} />
           </Card>
         </Col>
-        <Col span={6}>
-          <Card
-            title="Supported Board Types in use"
-            style={{ height: "100%" }}
-          >
-            <h2>Currently in use 4/27</h2>
-          </Card>
-        </Col>
-        <Col span={6}>
-          <Card
-            title="Most Used Board Types"
-            style={{ height: "100%" }}
-          >
-            <h2> 1. Arduino Mega 2. Arduino Due 3. Arduino lol </h2>
-          </Card>
-        </Col>
-        <Col span={6}>
-          <Card
-            title="S"
-            style={{ height: "100%" }}
-          >
-            INSERT GOOD INFORMATION
+        <Col span={12}>
+          <Card title="Graph B" style={{ height: "100%" }}>
+            <DeploymentsByTypeGraph data={graphData} />
           </Card>
         </Col>
       </Row>
-      <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 20]}  align="middle">
-      <Divider />
+
+      <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 20]} align="middle">
+        <Divider />
         <Col span={24}>
-          <List
-            style={{
-              overflow: "scroll",
-              overflowX: "hidden",
-              padding: "24px",
-              maxHeight: "calc(100% - 60px)",
-            }}
-            grid={{ gutter: 32 }}
-            dataSource={types}
-            renderItem={(type, index) => {
-              if (isType(type)) {
-                return (
-                  <List.Item key={type.id}>
-                    <Card
-                      size="small"
-                      hoverable
-                      style={{ width: 240 }}
-                      cover={
-                        <img
-                          alt={`${type.name} example image`}
-                          height={"100%"}
-                          src={MegaTest}
+          <Card>
+            <List
+              style={{
+                overflow: "scroll",
+                overflowX: "hidden",
+                padding: "24px",
+                maxHeight: "520px",
+              }}
+              grid={{ gutter: 32 }}
+              dataSource={types}
+              renderItem={(type, index) => {
+                if (isType(type)) {
+                  return (
+                    <List.Item key={type.id}>
+                      <Card
+                        size="small"
+                        hoverable
+                        style={{ width: 240 }}
+                        cover={
+                          <img
+                            alt={`${type.name} example image`}
+                            height={"100%"}
+                            src={MegaTest}
+                          />
+                        }
+                        actions={[<FilterOutlined />, <InfoOutlined />]}
+                      >
+                        <Meta
+                          title={type.name}
+                          description={"FQBN:" + type.fqbn}
+                          style={{ padding: "24px px" }}
                         />
-                      }
-                      actions={[<FilterOutlined />, <InfoOutlined />]}
-                    >
-                      <Meta
-                        title={type.name}
-                        description={"FQBN:"+type.fqbn}
-                        style={{ padding: "24px px" }}
-                      />
-                    </Card>
-                  </List.Item>
-                );
-              } else {
-                return (
-                  <List.Item key={`placeholder-${index}`}>
-                    <Card
-                      hoverable
-                      cover={
-                        <Skeleton.Image
-                          style={{ width: "300px", height: "220px" }}
-                        />
-                      }
-                      actions={[<SkeletonButton />, <SkeletonButton />]}
-                    >
-                      <Skeleton
-                        loading={true}
-                        paragraph={{ rows: 1 }}
-                      ></Skeleton>
-                    </Card>
-                  </List.Item>
-                );
-              }
-            }}
-          />
+                      </Card>
+                    </List.Item>
+                  );
+                } else {
+                  return (
+                    <List.Item key={`placeholder-${index}`}>
+                      <Card
+                        hoverable
+                        cover={
+                          <Skeleton.Image
+                            style={{ width: "300px", height: "220px" }}
+                          />
+                        }
+                        actions={[<SkeletonButton />, <SkeletonButton />]}
+                      >
+                        <Skeleton
+                          loading={true}
+                          paragraph={{ rows: 1 }}
+                        ></Skeleton>
+                      </Card>
+                    </List.Item>
+                  );
+                }
+              }}
+            />
+          </Card>
         </Col>
       </Row>
     </>
