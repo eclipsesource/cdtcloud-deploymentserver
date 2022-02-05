4b8f06046423824b6b9818190dc22c8694d19f57
Gozzim <80704304+Gozzim@users.noreply.github.com>
2022-02-05 13:22:35 +0100

    fix(DeploymentServerUI/Type): Fix number of devices of type

--- packages/deployment-server-ui/src/pages/TypeId.tsx
+++ packages/deployment-server-ui/src/pages/TypeId.tsx
@@ -1,53 +1,54 @@
-import { DeployRequest, Device, DeviceTypeResource } from "deployment-server";
-import { useEffect, useState } from "react";
-import defineFunctionalComponent from "../util/defineFunctionalComponent";
-import { Card, Col, Row, Spin, Statistic } from "antd";
-import { useInterval } from "react-use";
-import { useParams } from "react-router-dom";
-import { StatusTag } from "../components/StatusTag";
-import DeploymentsStatusGraph from "../components/Graphs/DeploymentsStatusGraph";
-import IssueCountGraph from "../components/Graphs/IssueCountGraph";
+import { DeployRequest, Device, DeviceTypeResource } from 'deployment-server'
+import { useEffect, useState } from 'react'
+import defineFunctionalComponent from '../util/defineFunctionalComponent'
+import { Card, Col, Row, Spin, Statistic } from 'antd'
+import { useInterval } from 'react-use'
+import { useParams } from 'react-router-dom'
+import { StatusTag } from '../components/StatusTag'
+import DeploymentsStatusGraph from '../components/Graphs/DeploymentsStatusGraph'
+import IssueCountGraph from '../components/Graphs/IssueCountGraph'
+import { DeviceStatus } from './Devices'
 
-const { Meta } = Card;
+const { Meta } = Card
 
 export default defineFunctionalComponent(function TypeId() {
-  const [loading, setLoading] = useState<boolean>(true);
-  const [devices, setDevices] = useState<Device[]>([]);
-  const [deviceType, setDeviceType] = useState<DeviceTypeResource>();
-  const [deployments, setDeployments] = useState<DeployRequest[]>([]);
-  const [refetchFlip, setRefetchFlip] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true)
+  const [devices, setDevices] = useState<Device[]>([])
+  const [deviceType, setDeviceType] = useState<DeviceTypeResource>()
+  const [deployments, setDeployments] = useState<DeployRequest[]>([])
+  const [deviceCount, setDeviceCount] = useState<number>(0)
+  const [refetchFlip, setRefetchFlip] = useState<boolean>(false)
 
-  const { id } = useParams();
+  const { id } = useParams()
+
   function findDeviceById(id: string) {
-    return devices.find((device) => id === device.id);
+    return devices.find((device) => id === device.id)
   }
 
   useInterval(function () {
-    setRefetchFlip(!refetchFlip);
-  }, 1000);
+    setRefetchFlip(!refetchFlip)
+  }, 1000)
 
   useEffect(() => {
     const fetchAsync = async () => {
-      const typesRes = await fetch(`/api/device-types/${id}`);
-      const types = await typesRes.json();
-      setDeviceType(types);
+      const typesRes = await fetch(`/api/device-types/${id}`)
+      const types = await typesRes.json()
+      setDeviceType(types)
 
-      const devRes = await fetch(`/api/devices`);
-      const devs = await devRes.json();
-      setDevices(devs.filter((dev: Device) => dev.deviceTypeId === id));
+      const devRes = await fetch(`/api/devices`)
+      const devs = await devRes.json()
+      setDevices(devs.filter((dev: Device) => dev.deviceTypeId === id))
 
-      const deployRes = await fetch("/api/deployments");
-      const deploys = await deployRes.json();
-      setDeployments(
-        deploys.filter(
-          (deployment: DeployRequest) =>
-            deployment.deviceId === findDeviceById(deployment.deviceId)?.id
-        )
-      );
-    };
+      const deployRes = await fetch('/api/deployments')
+      const deploys = await deployRes.json()
+      setDeployments(deploys.filter((deployment: DeployRequest) => deployment.deviceId === findDeviceById(deployment.deviceId)?.id))
 
-    fetchAsync();
-  }, []);
+      const amount = devices.filter((dev: Device) => dev.status !== DeviceStatus.UNAVAILABLE).length
+      setDeviceCount(amount)
+    }
+
+    fetchAsync()
+  }, [])
 
   useEffect(() => {
     if (
@@ -56,70 +57,70 @@ export default defineFunctionalComponent(function TypeId() {
       deployments != null &&
       loading
     ) {
-      setLoading(false);
+      setLoading(false)
     }
-  }, [devices, deviceType, deployments]);
+  }, [devices, deviceType, deployments])
 
   useEffect(() => {
     const fetchAsync = async () => {
-      const typesRes = await fetch(`/api/device-types/${id}`);
-      const types = await typesRes.json();
-      setDeviceType(types);
+      const typesRes = await fetch(`/api/device-types/${id}`)
+      const types = await typesRes.json()
+      setDeviceType(types)
 
-      const deployRes = await fetch("/api/deployments");
-      const deploys = await deployRes.json();
+      const deployRes = await fetch('/api/deployments')
+      const deploys = await deployRes.json()
       setDeployments(
         deploys.filter(
           (deployment: DeployRequest) =>
             deployment.deviceId === findDeviceById(deployment.deviceId)?.id
         )
-      );
-    };
+      )
+    }
 
-    fetchAsync();
-  }, [refetchFlip]);
+    fetchAsync()
+  }, [refetchFlip])
 
   const pendingCards = deployments
     .filter(
       (deployment) =>
-        deployment.status === "PENDING" && findDeviceById(deployment.deviceId)
+        deployment.status === 'PENDING' && findDeviceById(deployment.deviceId)
     )
     .map((deployment: DeployRequest) => (
       <div key={deployment.id}>
         <Row gutter={[0, 16]}>
-        <Card
-          bordered
-          style={{ width: 260 }}
-          cover={<StatusTag status={deployment.status} addIcon />}
-        >
-          <Meta  description={deployment.id } />
-        </Card>
+          <Card
+            bordered
+            style={{ width: 260 }}
+            cover={<StatusTag status={deployment.status} addIcon/>}
+          >
+            <Meta description={deployment.id}/>
+          </Card>
         </Row>
       </div>
-    ));
+    ))
 
   const currentCards = deployments
     .filter(
       (deployment) =>
-        deployment.status === "RUNNING" && findDeviceById(deployment.deviceId)
+        deployment.status === 'RUNNING' && findDeviceById(deployment.deviceId)
     )
     .map((deployment: DeployRequest) => (
       <div key={deployment.id}>
-       <Card
+        <Card
           bordered
           style={{ width: 260 }}
-          cover={<StatusTag status={deployment.status} addIcon />}
+          cover={<StatusTag status={deployment.status} addIcon/>}
         >
-          <Meta  description={deployment.id } />
+          <Meta description={deployment.id}/>
         </Card>
       </div>
-    ));
+    ))
 
   const otherCards = deployments
     .filter(
       (deployment) =>
-        deployment.status !== "PENDING" &&
-        deployment.status !== "RUNNING" &&
+        deployment.status !== 'PENDING' &&
+        deployment.status !== 'RUNNING' &&
         findDeviceById(deployment.deviceId)
     )
     .map((deployment: DeployRequest) => (
@@ -127,63 +128,63 @@ export default defineFunctionalComponent(function TypeId() {
         <Card
           bordered
           style={{ width: 260 }}
-          cover={<StatusTag status={deployment.status} addIcon />}
+          cover={<StatusTag status={deployment.status} addIcon/>}
         >
-          <Meta  description={deployment.id } />
+          <Meta description={deployment.id}/>
         </Card>
       </div>
-    ));
+    ))
 
   return (
     <main>
       {loading ? (
-        <Spin tip={"Loading..."} />
+        <Spin tip={'Loading...'}/>
       ) : (
         <div>
-          <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 16 }, 20]} align="middle">
+          <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 16 }, 20]} align={'middle'}>
             <Col span={9}>
               <Card
-                title={deviceType ? deviceType.name : ""}
-                style={{ height: "400px", overflow: "auto" }}
+                title={deviceType ? deviceType.name : ''}
+                style={{ height: '400px', overflow: 'auto' }}
               >
                 <Row>
                   <Col span={12}>
                     <Statistic
-                      title="Number of devices"
-                      value={deviceType ? deviceType.numberOfDevices : "0"}
-                      valueStyle={{ color: "black" }}
+                      title={'Number of devices'}
+                      value={deviceCount}
+                      valueStyle={{ color: 'black' }}
                     />
                   </Col>
                   <Col span={12}>
                     <Statistic
-                      title="Current Queue Length"
-                      value={deviceType ? deviceType.queueLength : "0"}
-                      valueStyle={{ color: "black" }}
+                      title={'Current Queue Length'}
+                      value={deviceType ? deviceType.queueLength : '0'}
+                      valueStyle={{ color: 'black' }}
                     />
                   </Col>
                 </Row>
                 <Row>
                   <Col span={24}>
                     <Statistic
-                      title="FQBN"
-                      value={deviceType ? deviceType.fqbn : "0"}
-                      valueStyle={{ color: "black" }}
+                      title={'FQBN'}
+                      value={deviceType ? deviceType.fqbn : '0'}
+                      valueStyle={{ color: 'black' }}
                     />
                   </Col>
                 </Row>
                 <Row>
                   <Col span={12}>
                     <Statistic
-                      title="Status"
-                      value={deviceType ? deviceType.status : ""}
-                      valueStyle={{ color: "black" }}
+                      title={'Status'}
+                      value={deviceType ? deviceType.status : ''}
+                      valueStyle={{ color: 'black' }}
                     />
                   </Col>
                   <Col span={12}>
                     <Statistic
-                      title="Device Type ID"
-                      value={deviceType ? deviceType.id : "0"}
-                      valueStyle={{ color: "black" }}
+                      title={'Device Type ID'}
+                      value={deviceType ? deviceType.id : '0'}
+                      valueStyle={{ color: 'black' }}
                     />
                   </Col>
                 </Row>
@@ -191,24 +192,24 @@ export default defineFunctionalComponent(function TypeId() {
             </Col>
             <Col span={5}>
               <Card
-                title="Pending Deployments"
-                style={{ height: "400px", overflow: "auto" }}
+                title={'Pending Deployments'}
+                style={{ height: '400px', overflow: 'auto' }}
               >
                 <Row>{pendingCards}</Row>
               </Card>
             </Col>
             <Col span={5}>
               <Card
-                title="Current Deployments"
-                style={{ height: "400px", overflow: "auto" }}
+                title={'Current Deployments'}
+                style={{ height: '400px', overflow: 'auto' }}
               >
                 <Row>{currentCards}</Row>
               </Card>
             </Col>
             <Col span={5}>
               <Card
-                title="Finished Deployments"
-                style={{ height: "400px", overflow: "auto" }}
+                title={'Finished Deployments'}
+                style={{ height: '400px', overflow: 'auto' }}
               >
                 <Row>{otherCards}</Row>
               </Card>
@@ -216,18 +217,18 @@ export default defineFunctionalComponent(function TypeId() {
           </Row>
           <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 16 }, 20]}>
             <Col span={12}>
-              <Card title="Deployments Status">
-                <DeploymentsStatusGraph data={deviceType!.history} />
+              <Card title={'Deployments Status'}>
+                <DeploymentsStatusGraph data={deviceType!.history}/>
               </Card>
             </Col>
             <Col span={12}>
-              <Card title="Issues Over Time">
-                <IssueCountGraph data={deviceType!.history} />
+              <Card title={'Issues Over Time'}>
+                <IssueCountGraph data={deviceType!.history}/>
               </Card>
             </Col>
           </Row>
         </div>
       )}
     </main>
-  );
-});
+  )
+})
