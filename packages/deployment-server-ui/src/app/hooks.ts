0b73506dd2530121c43b872f51f4117bc7d18f29
Kevin Kusterer <k.kusterer@tum.de>
2022-03-30 10:17:01 +0200

    fix(deployment-server-ui): use AppDispatch instead of Any type

--- packages/deployment-server-ui/src/app/hooks.ts
+++ packages/deployment-server-ui/src/app/hooks.ts
@@ -17,5 +17,5 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { AppDispatch, RootState } from './store'
 
-export const useAppDispatch = (): any => useDispatch<AppDispatch>()
+export const useAppDispatch = (): AppDispatch => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
